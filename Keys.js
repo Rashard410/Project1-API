4c9aa5c2f39758be9a8310a125f2e3dfd58e3aa3
Rashard410 <43615278+Rashard410@users.noreply.github.com>
2018-11-20 09:39:24 -0500

    Create Keys.js

---
+++ Keys.js
@@ -0,0 +1,15 @@
+{
+  "name": "language_translator",
+  "version": "1.0.0",
+  "description": "This program translates words from English to your desired language",
+  "main": "keys.js",
+  "dependencies": {
+    "watson-developer-cloud": "^3.13.0"
+  },
+  "devDependencies": {},
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "Rashard Brown Jr",
+  "license": "MIT"
+}
